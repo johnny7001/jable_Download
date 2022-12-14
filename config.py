3ee7bb1c02f89f68dae0c768450297d50c88038c
Tseng <theforeverwen@gmail.com>
2022-12-14 17:47:35 +0800

    下載所需要的套件

---
+++ config.py
@@ -0,0 +1,11 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+    'browser' : 'firefox' ,
+    'platform' : 'windows' ,
+    'mobile' : 'False'
+}
+# header = {
+# 'browser' : 'firefox' ,
+# 'platform' : 'windows' ,
+# 'mobile' : False
+# }
